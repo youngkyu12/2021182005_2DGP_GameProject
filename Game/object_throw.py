7f4e328c88b99975ddbd9077d5f34ad718655ace
YoungkyuThe_Two_domitory_803 <gopb12@daum.net>
2022-10-31 00:09:49 +0900

    Object 수정

--- Game/object_throw.py
+++ Game/object_throw.py
@@ -2,9 +2,6 @@ from pico2d import *
 
 Width, Height = 1280, 720
 
-import character
-
-
 class Object:
     def __init__(self):
         self.image = load_image('target_1_32x32.png')
@@ -17,14 +14,14 @@ class Object:
         self.throw_r = False
         self.throw_l = False
 
-    def update(self):
+    def update(self, cx, cy):
         self.t = self.i / 100
         if self.throw_l == True:
-            self.x = (2 * self.t ** 2 - 3 * self.t + 1) * character.Character().x + (-4 * self.t ** 2 + 4 * self.t) * (character.Character().x -((character.Character().x - self.xl) // 2)) + (2 * self.t ** 2 - self.t) * self.xl
-            self.y = (2 * self.t ** 2 - 3 * self.t + 1) * character.Character().y + (-4 * self.t ** 2 + 4 * self.t) * (character.Character().y + 100) + (2 * self.t ** 2 - self.t) * self.yl
+            self.x = (2 * self.t ** 2 - 3 * self.t + 1) * cx + (-4 * self.t ** 2 + 4 * self.t) * (cx -((cx - self.xl) // 2)) + (2 * self.t ** 2 - self.t) * self.xl
+            self.y = (2 * self.t ** 2 - 3 * self.t + 1) * cy + (-4 * self.t ** 2 + 4 * self.t) * (cy + 100) + (2 * self.t ** 2 - self.t) * self.yl
         elif self.throw_r == True:
-            self.x = (2 * self.t ** 2 - 3 * self.t + 1) * character.Character().x + (-4 * self.t ** 2 + 4 * self.t) * (character.Character().x + ((self.xr - character.Character().x) // 2)) + (2 * self.t ** 2 - self.t) * self.xr
-            self.y = (2 * self.t ** 2 - 3 * self.t + 1) * character.Character().y + (-4 * self.t ** 2 + 4 * self.t) * (character.Character().y + 100) + (2 * self.t ** 2 - self.t) * self.yr
+            self.x = (2 * self.t ** 2 - 3 * self.t + 1) * cx + (-4 * self.t ** 2 + 4 * self.t) * (cx + ((self.xr - cx) // 2)) + (2 * self.t ** 2 - self.t) * self.xr
+            self.y = (2 * self.t ** 2 - 3 * self.t + 1) * cy + (-4 * self.t ** 2 + 4 * self.t) * (cy + 100) + (2 * self.t ** 2 - self.t) * self.yr
 
         self.i += 4
         if self.i > 100:
@@ -35,11 +32,11 @@ class Object:
     def draw(self):
         self.image.draw(self.x, self. y)
 
-    def handle_events(self, event):
+    def handle_events(self, event, animation):
         if event.type == SDL_KEYDOWN:
             if event.key == SDLK_SPACE:
-                if character.animation == 5 or character.animation == 2:
+                if animation == 5 or animation == 2:
                     self.throw_l = True
-                elif character.animation == 4 or character.animation == 3:
+                elif animation == 4 or animation == 3:
                     self.throw_r = True
 
