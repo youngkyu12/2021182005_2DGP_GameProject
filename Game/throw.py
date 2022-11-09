de49d8bce809d9c1fa26e90a3a621bdf58b719c4
Youngkyu12 <gopb12@daum.net>
2022-11-09 23:54:32 +0900

    조건문 오류 수정

--- Game/throw.py
+++ Game/throw.py
@@ -3,25 +3,25 @@ Width, Height = 1280, 720
 import game_world
 class Throw:
     image = None
-    def __init__(self, x = Width // 2, y = Height // 2, animation = 3):
+    def __init__(self, x = Width // 2, y = Height // 2, action = 3):
         if Throw.image == None:
             Throw.image = load_image('target_1_32x32.png')
         self.x, self.y = 0, 0
         self.cx = x
         self.cy = y
-        self.animation = animation
+        self.action = action
         self.t = 0
-        self.xl, self.yl = 1280 - 1024 - 128, 720 - 600 + 100
-        self.xr, self.yr = 1280 - 128, 720 - 600 + 100
+        self.xl, self.yl = 1280 - 1024 - 128 - 20, 720 - 600 + 100
+        self.xr, self.yr = 1280 - 128 + 20, 720 - 600 + 100
         self.i = 0
 
 
     def update(self):
         self.t = self.i / 100
-        if self.animation == 0 or 2 or 5:
+        if self.action == 0 or self.action == 2 or self.action == 5:
             self.x = (2 * self.t ** 2 - 3 * self.t + 1) * self.cx + (-4 * self.t ** 2 + 4 * self.t) * (self.cx -((self.cx - self.xl) // 2)) + (2 * self.t ** 2 - self.t) * self.xl
             self.y = (2 * self.t ** 2 - 3 * self.t + 1) * self.cy + (-4 * self.t ** 2 + 4 * self.t) * (self.cy + 100) + (2 * self.t ** 2 - self.t) * self.yl
-        elif self.animation == 1 or 3 or 4:
+        elif self.action == 1 or self.action == 3 or self.action == 4:
             self.x = (2 * self.t ** 2 - 3 * self.t + 1) * self.cx + (-4 * self.t ** 2 + 4 * self.t) * (self.cx + ((self.xr - self.cx) // 2)) + (2 * self.t ** 2 - self.t) * self.xr
             self.y = (2 * self.t ** 2 - 3 * self.t + 1) * self.cy + (-4 * self.t ** 2 + 4 * self.t) * (self.cy + 100) + (2 * self.t ** 2 - self.t) * self.yr
 
@@ -29,7 +29,7 @@ class Throw:
         if self.i > 100:
             self.i = 0
 
-        if self.x < 1280 - 1024 - 128 + 20 or self.x > 1280 - 128 - 20:
+        if self.x < 1280 - 1024 - 128 or self.x > 1280 - 128:
             game_world.remove_object(self)
 
     def draw(self):
