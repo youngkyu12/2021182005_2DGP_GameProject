83ee16acf304fcb5587745006b9f044d47c833ab
Youngkyu12 <gopb12@daum.net>
2022-11-16 22:03:28 +0900

    esc 내부 기능(continue, retry, exit)

--- Game/esc_state.py
+++ Game/esc_state.py
@@ -3,8 +3,6 @@ import game_framework
 from pico2d import *
 from character import *
 
-#running = True
-bg_image = None
 esc_image = None
 pause_font = None
 continue_font = None
@@ -13,20 +11,23 @@ exit_font = None
 Key_check = None
 Width, Height = 1280, 720
 
+Box = [(Width // 2 - 50, Height // 2 + 30, Width // 2 + 60, Height // 2 + 10),
+       (Width // 2 - 50, Height // 2 - 20, Width // 2 + 15, Height // 2 - 40),
+       (Width // 2 - 50, Height // 2 - 70, Width // 2, Height // 2 - 90)]
+num = 0
+
 def enter():
     print('esc_state enter')
-    global bg_image, esc_image, pause_font, continue_font, exit_font, retry_font
+    global esc_image, pause_font, continue_font, exit_font, retry_font
     esc_image = load_image('esc_image.png')
     pause_font = load_font('ENCR10B.TTF', 30)
     continue_font = load_font('ENCR10B.TTF', 21)
     exit_font = load_font('ENCR10B.TTF', 21)
     retry_font = load_font('ENCR10B.TTF', 21)
 
-
 def exit():
     print('esc_state exit')
-    global bg_image, esc_image
-    del bg_image
+    global esc_image
     del esc_image
 
 
@@ -34,6 +35,7 @@ def update():
     pass
 
 def draw():
+    global num
     clear_canvas()
     main_state.draw_world()
     esc_image.draw(Width // 2, Height // 2)
@@ -41,9 +43,11 @@ def draw():
     continue_font.draw(Width // 2 - 50, Height // 2 + 20, "Continue", (0, 0, 0))
     retry_font.draw(Width // 2 - 50, Height // 2 - 30, "Retry", (0, 0, 0))
     exit_font.draw(Width // 2 - 50, Height // 2 - 80, "Exit", (0, 0, 0))
+    draw_rectangle(*Box[num])
     update_canvas()
 
 def handle_events():
+    global num
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -58,6 +62,34 @@ def handle_events():
             main_state.character.add_event(LU)
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_LEFT):
             main_state.character.add_event(LD)
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_UP):
+            num -= 1
+            if num < 0:
+                num += 1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_DOWN):
+            num += 1
+            if num > 2:
+                num -= 1
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+            if num == 0:
+                game_framework.pop_state()
+                pass
+            elif num == 1:
+                game_world.clear()
+                main_state.character = None
+                main_state.back = None
+                main_state.enemies1 = []
+                main_state.enemies2 = []
+                main_state.task1 = []
+                main_state.task2 = []
+                game_world.clear()
+                main_state.enter()
+                game_framework.pop_state()
+                pass
+            elif num == 2:
+                game_framework.quit()
+                pass
+
 def pause():
     print('esc_state pause')
     pass
